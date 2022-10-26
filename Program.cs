687db6ebcc87c6767ce9e84a670a54bf4b2460e6
Oleg <elbrus14@mail.ru>
2022-10-26 21:21:03 +0300

    finish task

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿string[] Array;
+Console.Write("Введите массив строк через , : ");
+string line = Console.ReadLine();
+Array = line.Split(",");
+var result = new string[Array.Length];
+var size = 0; //  var realSize = 0;
+foreach (var elements in Array)
+{
+    if (elements.Length <= 3)
+    {
+        result[size] = elements;
+        size++;
+    }
+}
+Console.Write(string.Join(Environment.NewLine, result, 0, size));
